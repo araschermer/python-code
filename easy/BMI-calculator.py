d3701cfc4f6f5aba6d69c9cc2cd1de7c20906fc4
abelkha <abdelkha@students.uni-marburg.de>
2021-03-05 19:41:07 +0100

    update BMI-calculator with weight-status estimation

--- easy/BMI-calculator.py
+++ easy/BMI-calculator.py
@@ -6,8 +6,25 @@ def calculate_body_mass_index(weight, height):
     # saves the whole number in the variable bmi
     # alternative: use the following to return a round the number to two digits
     #  mbi=round(weight/height**2,2)
-    print(bmi)
+    weight_status = ""
+    if bmi < 18.5:
+        weight_status = "underweight"
+    elif 18.5 <= bmi <= 25:
+        weight_status = "normal"
+    elif 25 < bmi <= 30:
+        weight_status = "overweight"
+    elif 30 < bmi <= 35:
+        weight_status = "obese"
+    elif bmi > 35:
+        weight_status = "clinically obese"
+
+    if weight_status == "normal":
+        print(f" your BMI is: {bmi}, your have a {weight_status} weight!")
+    else:
+        print(f" your BMI is: {bmi}, you are {weight_status}")
+
     return bmi
 
+
 if __name__ == '__main__':
-    calculate_body_mass_index(weight = 80, height = 1.75)
+    calculate_body_mass_index(weight = 66, height = 1.75)
