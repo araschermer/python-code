d5994018d0ae6e3b53520660cc1a4eee426b6f45
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-03-07 21:45:44 +0100

    updating caeser-cipher

--- 100 days of code/easy/caeser-cipher.py
+++ 100 days of code/easy/caeser-cipher.py
@@ -26,13 +26,6 @@ def decrypt(plain_text, shift_amount):
         text += alphabet[index]
     print(f"The decoded text is {text}")
 
-    # if direction=="encode":
-    encrypt(plain_text = text, shift_amount = shift)
-    # elif direction=="decode":
-    decrypt(plain_text = text, shift_amount = shift)
-    # else:
-    print("Wrong input ")
-
 
 # alternative solution
 def ceasar(plain_text, shift_amount, direction):
@@ -41,34 +34,48 @@ def ceasar(plain_text, shift_amount, direction):
         if letter in alphabet:
             index = alphabet.index(letter)
             if letter in alphabet:
-                if direction == "encode" or direction == 1:
+                if direction == "encode" or int(direction) == 1:
                     index += shift_amount
 
                     while index >= len(alphabet) - 1:
                         index = index % 25 - 1  # to start from index 0
 
-                if direction == "decode" or direction == 2:
+                if direction == "decode" or int(direction) == 2:
                     index -= shift_amount
                     while index < 0:
                         index += 25  # to start counting from z if the shifting is by a value> 25
                 text += alphabet[index]
         else:
             text += letter
+    if direction not in ['encode', 'decode'] or int(direction) in [1, 2]:
+        if int(direction) == 1:
+            print(f"the encoded text is {text}")
+        else:
+            print(f"the decoded text is {text}")
+    else:
+        print(f"the {direction}d text is {text}")
 
-    print(f"the {direction}d text is {text}")
+
+def get_message_details(direction=1):
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    ceasar(text, shift, direction)
 
 
 if __name__ == "__main__":
+    encrypt("hello",3)
+    decrypt("khoor",3)
     again = True
     while again:
-        direction = input("Type 'encode'  or 1 to encrypt, type 'decode' or 2 to decrypt:\n").lower()
-        if direction not in ['encode', 'decode', 1, 2]:
+        direction = input("Type 'encode', or 1 to encrypt. Type 'decode' or 2 to decrypt:\n").lower()
+        if direction not in ['encode', 'decode', '1', '2']:
             continue
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
+        else:
+            get_message_details()
 
-        ceasar(text, shift, direction)
         rerun = input("another run?  type yes to continue, or no to exit\n").lower()
-        if rerun == "no":
+        if rerun!="yes":
             again = not again
             print("Goodbye")
+        else: continue
+
