0b44d64a8f58e47344ab0c47071a674808769ac8
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-03-07 20:01:12 +0100

    updating password-generator

--- 100 days of code/easy/password-generator.py
+++ 100 days of code/easy/password-generator.py
@@ -14,7 +14,7 @@ def password_generator(num_lowercase=-1, num_uppercase=-1, num_numbers=-1, num_c
         num_uppercase = int(input("How many uppercase letters would you like in your password?\n"))
         num_numbers = int(input("How many numbers would you like in your password?\n"))
         num_characters = int(input("How many characters would you like in your password?\n"))
-    temp_password = ""  # to store the generated characters, letters and numbers string in the order of their generation
+    temp_password = []  # to store the generated characters, letters and numbers in the order of their generation
     password = ""  # to store the generated characters, letters and numbers string in a random order
     # generate the required number of lowercase,uppercase, numbers and characters
     # and store them in temp_password string
@@ -26,7 +26,6 @@ def password_generator(num_lowercase=-1, num_uppercase=-1, num_numbers=-1, num_c
         temp_password += choice(characters)
     for _ in range(num_numbers):
         temp_password += str(choice(numbers))
-    temp_password = list(temp_password)  # convert the temp_password  string to  a list
     shuffle(temp_password)  # shuffle the list
     password = password.join(temp_password)  # convert the shuffled list to a password string
     print(password)
