0d0ddf93513e92c8a2a6e08671c95a2618bc63b0
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-03-11 14:16:51 +0100

    adding us-states-game

---
+++ 100 days of code/intermediate/us-states-game/main.py
@@ -0,0 +1,34 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "us_states_map.gif"
+ALIGNMENT = 'center'
+FONT = ("Courier-Bold", 15, "normal")
+screen.addshape(image)
+turtle.shape(image)
+guessed_states = []
+states_data = pd.read_csv("us_states.csv")
+all_states = states_data.state.to_list()
+
+while len(guessed_states) < 50:
+    answer = screen.textinput(title = f"{len(guessed_states)}/50 State correct",
+                              prompt = "Guess a state's name").title()
+    if answer == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        missing_states_df = pd.DataFrame(missing_states)
+        missing_states_df.to_csv("states_to_learn.csv", index = False)
+        break
+    if answer in all_states:
+        guessed_states.append(answer)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        correct_state_data = states_data[states_data.state == answer]
+        t.goto(int(correct_state_data.x), int(correct_state_data.y))
+        t.write(answer,font = FONT, align = ALIGNMENT)
+screen.mainloop()
