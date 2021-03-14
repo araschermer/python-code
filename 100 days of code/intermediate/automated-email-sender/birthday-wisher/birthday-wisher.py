6b6e4640114636a5f60d56a5bdf8bfbe4ba9f622
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-03-14 16:18:04 +0100

    adding automated-email-sender to 100-days-of-code solutions

---
+++ 100 days of code/intermediate/automated-email-sender/birthday-wisher/birthday-wisher.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import pandas as pd
+from random import randint
+from smtplib import SMTP
+
+#
+SENDER_EMAIL = ""
+SENDER_PASSWORD = ""
+
+
+def get_smtp(sender_email):
+    if "yahoo" in sender_email.lower():
+        # SMTP_YAHOO = "smtp.mail.yahoo.com"
+        return "smtp.mail.yahoo.com"
+    elif "gmail" in sender_email.lower():
+        # SMTP_GMAIL = "smtp.gmail.com"
+        return "smtp.gmail.com"
+    elif "hotmail" in sender_email.lower():
+        # SMTP_HOTMAIL = "smtp.live.com"
+        return "smtp.live.com"
+    else:
+        print("SMTP for your email address is not defined")
+
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+# read the birthdays.csv
+data = pd.read_csv("birthdays.csv")
+# dictionary comprehension to create a dictionary from birthday.csv that is formatted like this:
+# birthdays_dict = {
+#     (birthday_month, birthday_day): data_row
+# }
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+# compare and see if today's month/day tuple matches one of the keys in birthday_dict
+# If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt)
+# from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
+if today_tuple in birthdays_dict:
+    file_path = f"letter_templates/letter_{randint(1, 3)}.txt"
+    with open(file_path) as letter_template:
+        birthday_person = birthdays_dict[today_tuple]
+        content = letter_template.read()
+        content = content.replace("[NAME]", birthday_person["name"].title())
+
+    # Sends the letter generated in content to that birthday_person's email address.
+    with SMTP(get_smtp(SENDER_EMAIL)) as connection:
+        connection.starttls()
+        connection.login(SENDER_EMAIL, SENDER_PASSWORD)
+        connection.sendmail(from_addr = SENDER_EMAIL, to_addrs = birthday_person["email"],
+                            msg = f"Subject:Happy Birthday!\n\n{content}")
---
+++ 100 days of code/intermediate/automated-email-sender/daily-motivational-quote.py
@@ -0,0 +1,58 @@
+import smtplib
+from random import choice
+import datetime as dt
+
+SENDER_EMAIL = ""  # to fill
+SENDER_PASSWORD = ""  # to fill
+RECEIVER_EMAIL = ""  # to fill
+
+
+def get_smtp(sender_email):
+    if "yahoo" in sender_email.lower():
+        # SMTP_YAHOO = "smtp.mail.yahoo.com"
+        return "smtp.mail.yahoo.com"
+    elif "gmail" in sender_email.lower():
+        # SMTP_GMAIL = "smtp.gmail.com"
+        return "smtp.gmail.com"
+    elif "hotmail" in sender_email.lower():
+        # SMTP_HOTMAIL = "smtp.live.com"
+        return "smtp.live.com"
+    else:
+        print("SMTP for your email address is not defined")
+
+
+def send_email(sender_email, password, receiver_email):
+    with smtplib.SMTP(get_smtp(sender_email)) as connection:
+        connection.starttls()
+        connection.login(user = sender_email, password = password)
+        connection.sendmail(from_addr = sender_email, to_addrs = receiver_email,
+                            msg = f"Subject: Motivational Motivational Quote\n\n{get_random_quote()}")
+
+
+def get_random_quote():
+    """return a random quote chosen from the quotes.txt file
+    :rtype string"""
+    with open("quotes.txt") as quotes:
+        lines = quotes.readlines()
+        return choice(lines)
+
+
+def get_current_date():
+    """Gets the current daytime
+     and returns the weekday (it can be adjusted to return more specific date info)
+    :rtype int"""
+    now = dt.datetime.now()
+
+    # year = now.year
+    # month = now.month
+    # day = now.day
+    weekday = now.weekday()
+    return weekday
+
+
+# send the email on a specific weekday (ignored; given the list [0...6])
+if get_current_date() in [0, 1, 2, 3, 4, 5, 6]:
+    send_email(sender_email = SENDER_EMAIL, password = SENDER_PASSWORD,
+               receiver_email = RECEIVER_EMAIL)
+else:
+    print("Something went wrong")
