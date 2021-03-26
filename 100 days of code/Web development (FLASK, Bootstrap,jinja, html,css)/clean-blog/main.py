5b69a4d3505efa6be9ec18af454426e79d2c15a5
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-03-26 18:30:01 +0100

    updating the contact form of clean-blog to send emails directly to an email saved in the environment variables using smtplib

--- 100 days of code/Web development (FLASK, Bootstrap,jinja, html,css)/clean-blog/main.py
+++ 100 days of code/Web development (FLASK, Bootstrap,jinja, html,css)/clean-blog/main.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+import smtplib
+
+from flask import Flask, render_template, request
 import requests
+import os
+
+EMAIL = os.environ.get('EMAIL')
+PASSWORD = os.environ.get('PASSWORD')
 
 posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
@@ -25,9 +31,27 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        message = request.form.get("message")
+        send_email(username, email, phone, message)
+        # in case the request.method == "POST":
+        return render_template("contact.html", message="Message sent ✔️")
+    # Otherwise when the  Contact me text"
+    return render_template("contact.html", message="Contact Me")
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(EMAIL, EMAIL, email_message)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
