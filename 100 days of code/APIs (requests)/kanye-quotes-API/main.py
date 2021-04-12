1db5905f2ea3c95c885dc899f0f025e3ff7f4849
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-04-12 10:30:45 +0200

    updating kanye-quotes-api and adding a readme file

--- 100 days of code/APIs (requests)/kanye-quotes-API/main.py
+++ 100 days of code/APIs (requests)/kanye-quotes-API/main.py
@@ -3,49 +3,60 @@ import requests
 from random import choice
 
 
-def get_quote(itemconfig=True):
+def get_quote() -> str:
+    """ returns a kanye quote either from the kanye.rest api or from the cached  quote in the data file"""
     try:
-        response = requests.get(url = "https://api.kanye.rest/")
-        print(response.status_code)
+        read_cached = False
+        response = requests.get(url="https://api.kanye.rest/")
+        # print(response.status_code)
         response.raise_for_status()
     except:
-        pass  # ignore any link/connection errors
+        read_cached = True
+        # ignore any link/connection errors, since it some quotes are cached that can be shown if an error occurs
 
     else:
         if response.status_code == 200:
             data = response.json()
+            # get the quote/text from the response
             text = data["quote"]
+            print(text)
             # caching the quotes to view in case no internet connection is available or a bad response is returned
             save_kanye_quotes(text)
-            if itemconfig:
-                canvas.itemconfig(quote_text, text = text)
+            #  update the canvas text with the new quote
+            canvas.itemconfig(quote_text, text=text)
             return text
-    with open('kanye_quotes.txt', 'r') as f:
-        kanye_quotes = f.readlines()
-        if itemconfig:
+    if read_cached:
+        with open('kanye_quotes.txt', 'r') as f:
+            kanye_quotes = f.readlines()
             text = choice(kanye_quotes)
-            canvas.itemconfig(quote_text, text = text)
-        return text
+            canvas.itemconfig(quote_text, text=text)
+            return text
 
 
-def save_kanye_quotes(kanye_quote):
+def save_kanye_quotes(kanye_quote: str):
+    """ save kanye quotes in a local file"""
     with open('kanye_quotes.txt', 'a') as f:
         f.write(f"{kanye_quote}\n")
 
 
+# create window
 window = Tk()
 window.title("Kanye Says...")
-window.config(padx = 50, pady = 50)
-
-canvas = Canvas(width = 300, height = 414)
-background_img = PhotoImage(file = "background.png")
-canvas.create_image(150, 207, image = background_img)
-quote_text = canvas.create_text(150, 207, width = 250, font = ("Arial", 20, "bold"), fill = "white")
-canvas.itemconfig(quote_text, text = get_quote())
-canvas.grid(row = 0, column = 0)
-
-kanye_img = PhotoImage(file = "kanye.png")
-kanye_button = Button(image = kanye_img, highlightthickness = 0, command = get_quote)
-kanye_button.grid(row = 1, column = 0)
+window.config(padx=50, pady=50)
+# create canvas
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+# add background_img to canvas
+canvas.create_image(150, 207, image=background_img)
+# add text field to show the quote
+quote_text = canvas.create_text(150, 207, width=250, font=("Arial", 20, "bold"), fill="white")
+# retrieve a random quote from the get_quote method
+canvas.itemconfig(quote_text, text=get_quote())
+# add canvas to the window grid
+canvas.grid(row=0, column=0)
+# create button
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
 
 window.mainloop()
---
+++ 100 days of code/APIs (requests)/kanye-quotes-API/readme.md
@@ -0,0 +1,87 @@
+<br />
+<p align="center">
+
+  <h3 align="center">KANYE-QUOES-API</h3>
+
+  <p align="center">
+    project_description
+    <br />
+Shows a random kanye west's quote
+   <br />
+    <br />
+  </p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+For his quotes fame, regardless of how much sense they make!, this project is to fetch kanye quotes from the kanye.rest api and  show them in a simple view in a tkinter window and caches the quotes for later need.
+## Example:
+'''Kanye Says:\
+I wish I had a friend like me'''
+
+### Built With
+* [Python](Python)
+
+### APIs used:
+*[kanye.rest](https://api.kanye.rest/)
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+1-download the project's file and install the requests module.\
+2- run the main.py script 
+
+### Installation
+
+1. Get the code from the project's file
+   [Kanye-quotes-API](https://github.com/amgad01/python-code/tree/main/100%20days%20of%20code/APIs%20(requests)/kanye-quotes-API)
+
+2. Install requests module
+   ```sh
+   $ python -m pip install requests
+   ```
+2. import the following modules in the projects file:
+```sh
+requests
+tkinter
+random
+```
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+showing random kanye quotes, regardless how much sense they make!
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
