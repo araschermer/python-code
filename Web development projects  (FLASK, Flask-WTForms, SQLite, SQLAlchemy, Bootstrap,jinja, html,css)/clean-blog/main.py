2a296e93acdb624ad898aac7968b964f83e01049
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-04-22 19:22:58 +0200

    updating clean-blog

--- Web development projects  (FLASK, Flask-WTForms, SQLite, SQLAlchemy, Bootstrap,jinja, html,css)/clean-blog/main.py
+++ Web development projects  (FLASK, Flask-WTForms, SQLite, SQLAlchemy, Bootstrap,jinja, html,css)/clean-blog/main.py
@@ -1,5 +1,4 @@
 import smtplib
-
 from flask import Flask, render_template, request
 import requests
 import os
@@ -7,19 +6,20 @@ import os
 EMAIL = os.environ.get('EMAIL')
 PASSWORD = os.environ.get('PASSWORD')
 
-posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_all_posts():
+    """ retrieves all blog posts through a request using the npoint api to retrieve the data from a given npoint page"""
+    posts = requests.get("https://api.npoint.io/0067e63917ca7a5034d9").json()
     return render_template("index.html", all_posts=posts)
 
 
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
+    posts = get_all_posts()
     for blog_post in posts:
         if blog_post["id"] == index:
             requested_post = blog_post
@@ -33,19 +33,20 @@ def about():
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
+    # when a message is submitted (post request)
     if request.method == "POST":
         username = request.form.get("username")
         email = request.form.get("email")
         phone = request.form.get("phone")
         message = request.form.get("message")
         send_email(username, email, phone, message)
-        # in case the request.method == "POST":
         return render_template("contact.html", message="Message sent ✔️")
-    # Otherwise when the  Contact me text"
+    # Otherwise renders the contact page "
     return render_template("contact.html", message="Contact Me")
 
 
 def send_email(name, email, phone, message):
+    """sends an email automatically with a name, email, phone  number and a message"""
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
