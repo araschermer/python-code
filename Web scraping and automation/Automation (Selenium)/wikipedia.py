51d1c6b86728c937512d66fb5b2ddcbe4b3e25c1
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-04-24 11:55:57 +0200

    updating wikipedia.py

--- Web scraping and automation/Automation (Selenium)/wikipedia.py
+++ Web scraping and automation/Automation (Selenium)/wikipedia.py
@@ -1,16 +1,17 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 url = "https://en.wikipedia.org/wiki/Wikipedia:Main_Page/1"
 CHROME_DRIVER = os.environ.get('CHROME_DRIVER')
-driver = webdriver.Chrome(executable_path = CHROME_DRIVER)
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER)
 driver.get(url)
-# getting the number rof articles (Articles count)
-articles_count=driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]')
+# getting the number of articles (Articles count)
+articles_count = driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]')
 # alternative
-# articles_count = driver.find_element_by_css_selector(
-#     "#articlecount a")  # articlecount is id, .something is for classes
-# print(articles_count.text)
+articles_count = driver.find_element_by_css_selector(
+    "#articlecount a")  # 'articlecount' is an id
+print(articles_count.text)
 
 # Finding elements by link text
 all_portals = driver.find_element_by_link_text("All portals")
@@ -18,7 +19,6 @@ all_portals.click()
 
 # Finding elements by name
 search = driver.find_element_by_name("search")
-search.send_keys("Python")
-from selenium.webdriver.common.keys import Keys
-search.send_keys(Keys.ENTER)
+search.send_keys("Python")  # to type python in the search bar
+search.send_keys(Keys.ENTER)  # to click enter and search the keyword
 driver.close()
