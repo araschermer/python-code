2286c756aaebd04fd7118d1bf2e1fa652ff6fbec
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-05-02 20:59:09 +0200

    adding util.py  containing help-functions

--- algorithms_and_data_structures/util.py
+++ algorithms_and_data_structures/util.py
@@ -6,6 +6,25 @@ def time_it(func):
         start = time()
         operation = func(*args, **kwargs)
         end = time()
-        print(f"Runtime of {func.__name__} is: {(end - start)*1000} ms")
+        print(f"Runtime of {func.__name__} is: {(end - start) * 1000} ms")
         return operation
+
     return wrapper
+
+
+def check_validity(nums, target=0, repetitive_numbers=False, variable_types=int):
+    # Check valid target:
+    if variable_types is None:
+        variable_types = []
+    if type(target) is not int:
+        raise TypeError('Target is not an integer!')
+    if nums is None or nums ==[]:
+        raise TypeError("'NoneType' object is not iterable")
+    # check the type of the nums elements :
+    for index, num in enumerate(nums):
+        if type(num) not in variable_types:
+            raise TypeError(f"{num} Invalid number type in the input array")
+        # check non-repetitiveness in the array
+        if not repetitive_numbers:
+            if num in nums and nums.index(num) != index:
+                raise ValueError("Array contains repetitive numbers")
