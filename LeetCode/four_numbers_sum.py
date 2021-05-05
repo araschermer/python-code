e049398afb9cbbfe122b4c8d42e82af157836f7d
abelkha <44481677+abdelkha@users.noreply.github.com>
2021-05-05 11:48:55 +0200

    adding basic tests to four_numbers_sum.py

--- LeetCode/four_numbers_sum.py
+++ LeetCode/four_numbers_sum.py
@@ -1,3 +1,6 @@
+from numpy.testing import assert_equal
+
+
 def four_number_sum(nums: [int], target) -> [[int]]:
     """Returns all possible combinations (without duplicates) of all elements in nums that sum up to target.
     """
@@ -28,15 +31,14 @@ def four_number_sum(nums: [int], target) -> [[int]]:
     return results
 
 
-print(four_number_sum([-1, -5, -5, -3, 2, 5, 0, 4], -7))
-print(four_number_sum([-3, -2, -1, 0, 0, 1, 2, 3], 0))
-# print(four_number_sum([7, 6, 4, -1, 1, 2], 16))
-# print(four_number_sum([-1, 1, 1, -2, 0, 7, 6, 4, -1, 1, 2], 0))
-
-# print(four_number_sum([7, 6, 4, -1, 1, 2], 16))
-# print(four_number_sum([-1, 1, 1, -2, 0, 7, 6, 4, -1, 1, 2], 0))
-# print(four_number_sum([1, 0, -1, 0, -2, 2], 0))
-# print(four_number_sum([2, 2, 2, 2, 2], 8))
-# print(four_number_sum([-3, -1, 0, 2, 4, 5], 0))
-# print(four_number_sum([-3, -2, -1, 0, 0, 1, 2, 3], 0))
-# print(four_number_sum([-2, -1, -1, 1, 1, 2, 2], 0))
+assert_equal(four_number_sum([-1, -5, -5, -3, 2, 5, 0, 4], -7), [[-5, -3, -1, 2], [-5, -5, -1, 4]])
+assert_equal(four_number_sum([-3, -2, -1, 0, 0, 1, 2, 3], 0), [[-2, -1, 0, 3], [-1, 0, 0, 1], [-2, 0, 0, 2],
+                                                               [-3, 0, 0, 3], [-2, -1, 1, 2], [-3, 0, 1, 2],
+                                                               [-3, -1, 1, 3], [-3, -2, 2, 3]])
+assert_equal(four_number_sum([7, 6, 4, -1, 1, 2], 16), [[1, 2, 6, 7], [-1, 4, 6, 7]])
+assert_equal(four_number_sum([-1, 1, 1, -2, 0, 7, 6, 4, -1, 1, 2], 0), [[-2, -1, -1, 4], [-1, -1, 0, 2], [-1, -1, 1, 1],
+                                                                        [-2, 0, 1, 1], [-2, -1, 1, 2]])
+assert_equal(four_number_sum([1, 0, -1, 0, -2, 2], 0), [[-1, 0, 0, 1], [-2, 0, 0, 2], [-2, -1, 1, 2]])
+assert_equal(four_number_sum([2, 2, 2, 2, 2], 8), [[2, 2, 2, 2]])
+assert_equal(four_number_sum([-3, -1, 0, 2, 4, 5], 0), [[-3, -1, 0, 4]])
+assert_equal(four_number_sum([-2, -1, -1, 1, 1, 2, 2], 0), [[-1, -1, 1, 1], [-2, -1, 1, 2]])
